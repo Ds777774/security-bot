24b7891a19dc48927c0c717fe4d4c44dc16a6844
Ds777774 <sjemma819@gmail.com>
2025-02-03 21:11:05 +0530

    Update germanD.js

--- germanD.js
+++ germanD.js
@@ -1,1 +1,29 @@
-1
+const germanQuizData = [
+    {
+        word: "Haus",
+        options: ["House", "Car", "Tree", "River"],
+        correct: "House"
+    },
+    {
+        word: "Freund",
+        options: ["Friend", "Enemy", "Brother", "Teacher"],
+        correct: "Friend"
+    },
+    {
+        word: "Tisch",
+        options: ["Table", "Chair", "Bed", "Door"],
+        correct: "Table"
+    },
+    {
+        word: "Buch",
+        options: ["Book", "Pen", "Notebook", "Paper"],
+        correct: "Book"
+    },
+    {
+        word: "Schule",
+        options: ["School", "Hospital", "Office", "Shop"],
+        correct: "School"
+    }
+];
+
+module.exports = { germanQuizData };
