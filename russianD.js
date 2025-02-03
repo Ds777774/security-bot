f9d89b2b8649c06a2b8cffdfe4c9e6cd0b3bd897
Ds777774 <sjemma819@gmail.com>
2025-02-03 21:12:03 +0530

    Update russianD.js

--- russianD.js
+++ russianD.js
@@ -1,1 +1,29 @@
-1
+const russianQuizData = [
+    {
+        word: "Книга",
+        options: ["Book", "Pen", "Paper", "Notebook"],
+        correct: "Book"
+    },
+    {
+        word: "Стол",
+        options: ["Table", "Chair", "Sofa", "Bed"],
+        correct: "Table"
+    },
+    {
+        word: "Машина",
+        options: ["Car", "Train", "Bus", "Bicycle"],
+        correct: "Car"
+    },
+    {
+        word: "Собака",
+        options: ["Dog", "Cat", "Mouse", "Rabbit"],
+        correct: "Dog"
+    },
+    {
+        word: "Школа",
+        options: ["School", "University", "Office", "Hospital"],
+        correct: "School"
+    }
+];
+
+module.exports = { russianQuizData };
